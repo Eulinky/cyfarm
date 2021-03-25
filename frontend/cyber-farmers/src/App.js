6f8f76d97758572f25d35f2cd97674a2590b47d7
Rainer Körner <rainer.koerner@mailbox.org>
2021-03-25 10:19:10 +0100

    Bugfix nullref donations

--- frontend/cyber-farmers/src/App.js
+++ frontend/cyber-farmers/src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
     showProject: false,
     showNotificationBar: true,
     error: null,
-    userInfo: { accountName: '', eosBalance: '', bondTokens: [] },
+    userInfo: { accountName: '', eosBalance: null, bondTokens: [], donations: [], goods: [], keys: [] },
     projects: []
   }
 

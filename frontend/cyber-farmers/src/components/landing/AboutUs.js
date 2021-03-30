21fa9afcf8e8f0453dbf6712ea7f31750a3de727
Rainer Körner <rainer.koerner@mailbox.org>
2021-03-30 10:13:42 +0200

    Text adjustments About Us

--- frontend/cyber-farmers/src/components/landing/AboutUs.js
+++ frontend/cyber-farmers/src/components/landing/AboutUs.js
@@ -11,11 +11,17 @@ class AboutUs  extends React.Component {
           <div className="intro__inner row__1200">
 
             <div className="intro__content" data-aos="fade-up" >
-              <h2 className="eksell">How does it work?</h2>
+              <h2 className="eksell">Why is it cool?</h2>
               <p>
-                We work with local Organizations to vet farmers in need and connected them with donors. Donors will receive a BOND token that will eventually be bought back by the farmer by spreading the good to other people in their community.
+                Cyber Farmer is a fundraising platform where the community can support local businesses in emergency situations like the Covid crisis.
+              </p>
+              <p>
+                Unlike existing donation platforms, we require a strong commitment from companies seeking help to honor a donation in the future, once the crisis has been overcome. In return, all donors receive symbolic bonds for their donations, entitling them to some kind of future compensation.
+              </p>
+              <p>
+              We believe it's time to take the donation process to a new level. 
 
-                <a href="about-us.html">Learn more</a>
+              CYFAR bond tokens create a more sustainable relationship between donors and donation receivers, building a vibrant foundation for local community engagement and the awareness of existential dynamics in today's society.
               </p>
             </div>
           </div>
@@ -27,28 +33,28 @@ class AboutUs  extends React.Component {
               <li className="home-skills__li tilt-img" data-aos="fade-right" data-aos-duration="800">
                 <a href="#projects" className="animateanchor">
                   <h2 className="eksell">1 - You donate <span className="arrow-pointer"></span></h2>
-                  <p>Your donate to a project and receive a BOND token. The BOND represent a social obligation for the farmer to pay back.</p>
+                  <p>Your donate to a project, the money goes straight to the farmer. You receive CYFAR bond tokens which act as a receipt or claim for future compensation.</p>
                 </a>
               </li>
             
               <li className="home-skills__li tilt-img" data-aos="fade-left" data-aos-duration="800">
                 <a href="#projects" className="animateanchor">
-                  <h2 className="eksell">2 - The Farmer invest <span className="arrow-pointer"></span></h2>
-                  <p> Your donations will be used by farmers to invest in new equipment that will increase their production.</p>
+                  <h2 className="eksell">2 - The Farmer invests <span className="arrow-pointer"></span></h2>
+                  <p> Your donation will be put to the best use by the farmer. Companies themselves know best where the money will help, which is why we don't make any rules here. </p>
                 </a>
               </li>
               
               <li className="home-skills__li tilt-img" data-aos="fade-right" data-aos-duration="800">
                 <a href="#projects" className="animateanchor">
                   <h2 className="eksell">3 - Harvest time <span className="arrow-pointer"></span></h2>
-                  <p>The Farmer can either send a rewards to donors, or transfer the bond by helping someone in their community (Ex: by sharing part of the harvest).</p>
+                  <p>Once the farmer's business is better, he or she can offer compensations on our market, which are exclusively redeemable by their supporters using the CYFAR bond tokens.</p>
                 </a>
               </li>
               
               <li className="home-skills__li tilt-img" data-aos="fade-left" data-aos-duration="800">
                 <a href="#projects" className="animateanchor">
                   <h2 className="eksell">Everybody wins! <span className="arrow-pointer"></span></h2>
-                  <p>Everyone wins! The donor receive a rewards and its initial bonds is transfered across the comunity many times while everyone help each other.</p>
+                  <p>CYFAR tokens lead to more community engagement, helping people and businesses alike. CYFAR tokens can be transferred between people at will, allowing more in the future than just redeeming some goods. </p>
                 </a>
               </li>
             </ul>  
